7796f3564c74c3b9157adae07cd66d3aaf96c6a8
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-01 13:50:39 +0530

    while loop

---
+++ 06_whileLoop.py
@@ -0,0 +1,14 @@
+# i=0
+# while(i<3):
+#     print(i)
+#     i=i+1
+
+# print("Work has been done")
+
+i=int(input("Enter the number"))
+
+while(i<=40):
+    i=int(input("Enter the number"))
+    print(i)
+else:   
+    print("Work DOne")
