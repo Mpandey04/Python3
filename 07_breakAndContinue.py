e06aa2743ad07b7c75770aff12aa2d4e3c55c5ce
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-01 13:50:53 +0530

    break and continue

---
+++ 07_breakAndContinue.py
@@ -0,0 +1,24 @@
+# for i in range(1,101,1):
+#     print(i,end=",")
+#     if(i==50):
+#         break
+
+# print("Work has been done")
+
+
+# for i in range(12):
+#     if(i==10):
+#         print("Skipping this iteration")
+#         continue
+#     print("5 X",(i+1),"=",5*(i+1))
+
+
+i=0;
+while True:
+    print(i)
+    i=i+1;
+    if(i==100):
+        break
+    
+print("Condition false and loop exist")
+    
