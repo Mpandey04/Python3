b82470f4e3d30a7c5c4a67011ab476ea1b67acf5
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-02 16:42:14 +0530

    fString

---
+++ 11_fstring.py
@@ -0,0 +1,11 @@
+name="Manish Kumar pandey"
+country="India"
+letter="Hey my name is {1} and I am from {0}"
+print(letter.format(name,country))
+
+print(f"Hey my Name is:{name} and i am from {country}")
+
+price=124.556789
+print(f"Price of petrol is:{price:.2f}")
+
+print(f"First name is {{name}}")
