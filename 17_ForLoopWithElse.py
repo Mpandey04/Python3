2f7239ab41a56ff32952ba4a49c3895b4fc3441e
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-03 09:53:08 +0530

    ForLoopwithElse

---
+++ 17_ForLoopWithElse.py
@@ -0,0 +1,17 @@
+for i in range(1,11,2):
+    print(i)
+else:
+    print("Sorry no I")
+    
+    
+for i in []:
+    print(i)
+else:
+    print("Empty list")
+    
+for i in range(6):
+    print(i)
+    if i==4:
+        break
+else:
+    print("Loop termintated")
