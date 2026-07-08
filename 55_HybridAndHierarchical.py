ade6b062b0cea937033b2c06b29fc77989bd2f72
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-08 18:07:04 +0530

    HybridAndHierrarchical

---
+++ 55_HybridAndHierarchical.py
@@ -0,0 +1,13 @@
+#Example of Hybrid Inheritance
+# class A:
+#     pass
+# class B(A):
+#     pass
+# class C(B):
+#     pass
+# class D(C,B):
+#     pass
+
+
+
+#Example of Hierarchical Inheritance
