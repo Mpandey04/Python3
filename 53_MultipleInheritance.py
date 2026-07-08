2782cd823b54d7030493f882d3889e21f498d69d
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-08 17:04:40 +0530

    multiple inheritance

---
+++ 53_MultipleInheritance.py
@@ -0,0 +1,22 @@
+class Employee:
+    def __init__(self,name):
+        self.name=name
+    def show(self):
+        print(f"The name is: {self.name}")
+
+class Dancer:
+    def __init__(self,dance):
+        self.dance=dance
+    def show(self):
+        print(f"Dance is :{self.dance}")
+        
+class DancerEmployee(Dancer,Employee):
+    def __init__(self, dance,name):
+        self.dance=dance
+        self.name=name
+        
+o=DancerEmployee("Kathak","shivani")
+print(o.name)
+print(o)
+o.show()
+print(o.dance)
