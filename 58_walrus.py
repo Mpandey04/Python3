8e28a468f3ffa7578865f6a157085b4016db5e5a
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-08 19:55:53 +0530

    walrus

---
+++ 58_walrus.py
@@ -0,0 +1,11 @@
+# a=True
+# print(a:=False)
+
+
+numbers=[1,2,3,4,5]
+while (n:=len(numbers))>0:
+    print(numbers.pop())
+    
+foods=list()
+while (food :=input("what food you do like?")) !="quit":
+    foods.append(foods)
