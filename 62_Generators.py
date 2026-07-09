699b5ecd839ff6fb082ec74f2c6e41ff1ffd43c7
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-09 17:38:41 +0530

    Generator

---
+++ 62_Generators.py
@@ -0,0 +1,11 @@
+def my_generator():
+    for i in range(50000000):
+        yield i
+
+gen=my_generator()
+print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+# for j in gen:
+#     print(j)
